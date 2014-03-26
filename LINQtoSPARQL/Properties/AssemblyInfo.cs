2df2b17e852e34713e56f747ea6ff676d321d361
Efimster <efimster@gmail.com>
2014-03-26 09:27:34 +0100

    project information minor changes

--- LINQtoSPARQL/Properties/AssemblyInfo.cs
+++ LINQtoSPARQL/Properties/AssemblyInfo.cs
@@ -6,7 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LINQtoSPARQL")]
-[assembly: AssemblyDescription("LINQtoSPARQL is a specific LINQ for making SPARQL queries to RDF sources")]
+[assembly: AssemblyDescription(@"LINQtoSPARQL is specific LINQ to SPARQL. Dynamically scaffolds SPARQL queries by using LINQ style.
+Built on top of DynamicSPARQL project https://github.com/Efimster/DynamicSPARQL/wiki.
+Please visit project wiki pages https://github.com/Efimster/LINQtoSPARQL/wiki . 
+")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Efimster")]
 [assembly: AssemblyProduct("LINQtoSPARQL")]
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 LINQtoSPARQL is specific LINQ to SPARQL. Dynamically scaffolds SPARQL queries by using LINQ style.
 Built on top of DynamicSPARQL project https://github.com/Efimster/DynamicSPARQL/wiki.
 Please visit project wiki pages https://github.com/Efimster/LINQtoSPARQL/wiki . 
+
+Available through NuGet https://www.nuget.org/packages/LINQtoSPARQL/
